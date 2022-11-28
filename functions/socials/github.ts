9a222fb63dd83d43a40536a529f7fe701653f35f
navhits <naveen.sunilkumar@freshworks.com>
2022-11-28 23:41:42 +0530

    Added talk section

--- functions/socials/github.ts
+++
@@ -1,6 +0,0 @@
-const destinationURL = 'https://github.com/navhits';
-const statusCode = 301;
-
-export async function onRequestGet() {
-    return new Response.redirect(destinationURL, statusCode);
-}
--- functions/socials/instagram.ts
+++
@@ -1,6 +0,0 @@
-const destinationURL = 'https://instagram.com/nav_s15';
-const statusCode = 301;
-
-export async function onRequestGet() {
-    return new Response.redirect(destinationURL, statusCode);
-}
